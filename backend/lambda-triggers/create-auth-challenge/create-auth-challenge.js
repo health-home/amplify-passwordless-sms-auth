581c5b4476d0b601481dd6e7e2d08f281f22d496
Antony <59047223+antony-experion@users.noreply.github.com>
2020-04-23 13:48:58 +0530

    Update create-auth-challenge.js

--- backend/lambda-triggers/create-auth-challenge/create-auth-challenge.js
+++ backend/lambda-triggers/create-auth-challenge/create-auth-challenge.js
@@ -49,6 +49,6 @@ exports.handler = async (event = {}) => {
 
 // Send secret code over SMS via Amazon Simple Notification Service (SNS)
 async function sendSMSviaSNS(phoneNumber, passCode) {
-    const params = { "Message": "[MobileQuickie] Your secret code: " + passCode, "PhoneNumber": phoneNumber };
+    const params = { "Message": "<#> WeCare - Your secret code: " + passCode + " FDyNaTSHPn3", "PhoneNumber": phoneNumber };
     await sns.publish(params).promise();
 }
